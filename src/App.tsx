33c6a4b81526282b04fc6b3bc1782fc985f985b5
FrogUni <80472206+danpung628@users.noreply.github.com>
2025-10-24 01:40:20 +0900

    feat: Tailwind CSS 설치 및 설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,19 @@ import Parking from "./pages/Parking";
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">홈</Link> |<Link to="/crowd">인파</Link> |
-        <Link to="/transit">교통</Link> |<Link to="/parking">주차</Link>
+      <nav className="bg-blue-500 text-white p-4 flex gap-4">
+        <Link to="/" className="hover:underline">
+          홈
+        </Link>
+        <Link to="/crowd" className="hover:underline">
+          인파
+        </Link>
+        <Link to="/transit" className="hover:underline">
+          교통
+        </Link>
+        <Link to="/parking" className="hover:underline">
+          주차
+        </Link>
       </nav>
 
       <Routes>
